616d46148ab886b6ececd9e3948d8a404179aab7
Justin Obara <obarajustin@gmail.com>
2015-07-03 15:32:27 -0400

    FLOE-399: Partial commit, moving % calculations
    Have started to move the percentage calculations into their own file. Need to make use of these new/refactored methods in dataEntry and dataEntryPanel

---
+++ src/js/percentage.js
@@ -0,0 +1,50 @@
+/*!
+Copyright 2015 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://github.com/gpii/universal/LICENSE.txt
+*/
+
+(function ($, fluid) {
+
+    "use strict";
+
+    fluid.registerNamespace("gpii.chartAuthoring.percentage");
+
+    gpii.chartAuthoring.percentage.calculate = function (value, total) {
+        value = parseFloat(value, 10) || 0;
+        total = parseFloat(total, 10) || 1;
+        return (value / total) * 100;
+    };
+
+    gpii.chartAuthoring.percentage.retrieveVal = function (val) {
+        return typeof(val) === "function" ? val() : val;
+    };
+
+    /**
+     * Renders the percentage into a DOM element.
+     * @param elm - a jQueryable selector representing the element(s) to set the percentage on
+     * @param perencentage {Number/String/Function} - Either a Number/String representation of the
+     * perencentage, or a function that returns it. *Note: No arguments are passed into the function*
+     * @param template {String} - an optional string template to use for rendering the percentage. By
+     * default only the percentage value is rendered.
+     */
+    gpii.chartAuthoring.percentage.render = function (elm, percentage, template) {
+        elm = $(elm);
+        template = template || "%percentage";
+        percentage = gpii.chartAuthoring.percentage.retrieveVal(percentage);
+
+        var output = fluid.stringTemplate(template, {percentage: percentage});
+        elm.text(output);
+    };
+
+    // used to provide a default value if a model or someother value had not been set.
+    gpii.chartAuthoring.percentage.percentageIfValue = function (percentage, value, defPercentage) {
+        defPercentage = defPercentage || "";
+        return !fluid.isValue(value) ? defPercentage : gpii.chartAuthoring.percentage.retrieveVal(percentage);
+    };
+
+})(jQuery, fluid);
---
+++ tests/js/percentageTests.js
@@ -0,0 +1,128 @@
+/*!
+Copyright 2015 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://github.com/gpii/universal/LICENSE.txt
+*/
+
+(function ($, fluid) {
+
+    "use strict";
+
+    fluid.registerNamespace("gpii.tests.calculatePercentage");
+
+    gpii.tests.calculatePercentage.inputs = [undefined, null, NaN, false, true, function () {}, {}, ["array"], "", "string", 2.2, "2.2", 0, "0", 50, "50", 100, "100"];
+    gpii.tests.calculatePercentage.outputs = [
+        // undefined, null, NaN, false, true, function, {}, [], "", "string", 2.2, "2.2", 0, "0", 50, "50", 100, "100"]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === undefined
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === null
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === NaN
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === false
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === true
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === function () {}
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === {}
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === []
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === ""
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === "string"
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 100, 0, 0, 2272.7272727272725, 2272.7272727272725, 4545.454545454545, 4545.454545454545], // total === 2.2
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 100, 0, 0, 2272.7272727272725, 2272.7272727272725, 4545.454545454545, 4545.454545454545], // total === "2.2"
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === 0
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 220, 0, 0, 5000, 5000, 10000, 10000], // total === "0"
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4.4, 4.4, 0, 0, 100, 100, 200, 200], // total === 50
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4.4, 4.4, 0, 0, 100, 100, 200, 200], // total === "50"
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.2, 2.2, 0, 0, 50, 50, 100, 100], // total === 100
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2.2, 2.2, 0, 0, 50, 50, 100, 100] // total === "100"
+    ];
+
+    jqUnit.test("Test gpii.chartAuthoring.percentage.calculate", function () {
+        fluid.each(gpii.tests.calculatePercentage.inputs, function (total, totalIdx) {
+            fluid.each(gpii.tests.calculatePercentage.inputs, function (value, valIdx) {
+                var actual = gpii.chartAuthoring.percentage.calculate(value, total);
+                var expected = gpii.tests.calculatePercentage.outputs[totalIdx][valIdx];
+                // Using fluid.model.isSameValue to remove any javascript precision errors on floating point numbers.
+                jqUnit.assertTrue("The percentate for value: " + value + " total: " + total + " should be calculated as " + expected, fluid.model.isSameValue(expected, actual));
+            });
+        });
+    });
+
+    fluid.registerNamespace("gpii.tests.retrieveVal");
+
+    gpii.tests.retrieveVal.perenctages = [
+        "10.5",
+        10.5,
+        "10",
+        10,
+        function () {return "10.5";},
+        function () {return 10.5;},
+        function () {return "10";},
+        function () {return 10;}
+    ];
+
+    gpii.tests.retrieveVal.perenctages = ["10.5", 10.5, "10", 10, "10.5", 10.5, "10", 10];
+
+    jqUnit.test("Test gpii.chartAuthoring.percentage.retrieveVal", function () {
+        fluid.each(gpii.tests.retrieveVal.perenctages, function (percentage, idx) {
+            jqUnit.assertEquals("The value should have been retrieved correctly", gpii.tests.retrieveVal.perenctages[idx], gpii.chartAuthoring.percentage.retrieveVal(percentage));
+        });
+    });
+
+    fluid.registerNamespace("gpii.tests.renderPerencentage");
+
+    gpii.tests.renderPerencentage.templates = [undefined, "%percentage%"];
+
+    gpii.tests.renderPerencentage.outputs = [
+        ["10.5", "10.5", "10", "10", "10.5", "10.5", "10", "10"], // template === undefined
+        ["10.5%", "10.5%", "10%", "10%", "10.5%", "10.5%", "10%", "10%"] // template === "%percentage%"
+    ];
+
+    jqUnit.test("Test gpii.chartAuthoring.percentage.render", function () {
+        var elm = $(".renderPerencentage-test");
+        fluid.each(gpii.tests.renderPerencentage.templates, function (template, templateIdx) {
+            fluid.each(gpii.tests.retrieveVal.perenctages, function (percentage, perIdx) {
+                // make sure the text is reset
+                elm.text("");
+
+                var expected = gpii.tests.renderPerencentage.outputs[templateIdx][perIdx];
+                gpii.chartAuthoring.percentage.render(elm, percentage, template);
+                var actual = elm.text();
+                jqUnit.assertEquals("The percentage should be rendered into the DOM correctly.", expected, actual);
+
+                // cleanup after test
+                elm.text("");
+            });
+        });
+    });
+
+    fluid.registerNamespace("gpii.tests.percentagesIfValue");
+
+    gpii.tests.percentagesIfValue.values = [undefined, null, NaN, "", false, true, 0, "0", 10];
+    gpii.tests.percentagesIfValue.outputs = [
+        ["", "", "", "", "", "", "", ""], // value === undefined
+        ["", "", "", "", "", "", "", ""], // value === null
+        ["10.5", "10.5", "10", "10", "10.5", "10.5", "10", "10"], // value === NaN
+        ["10.5", "10.5", "10", "10", "10.5", "10.5", "10", "10"], // value === ""
+        ["10.5", "10.5", "10", "10", "10.5", "10.5", "10", "10"], // value === false
+        ["10.5", "10.5", "10", "10", "10.5", "10.5", "10", "10"], // value === true
+        ["10.5", "10.5", "10", "10", "10.5", "10.5", "10", "10"], // value === 0
+        ["10.5", "10.5", "10", "10", "10.5", "10.5", "10", "10"], // value === "0"
+        ["10.5", "10.5", "10", "10", "10.5", "10.5", "10", "10"], // value === 10
+    ];
+
+    jqUnit.test("Test gpii.chartAuthoring.percentage.percentageIfValue", function () {
+        fluid.each(gpii.tests.percentagesIfValue.values, function (value, valIdx) {
+            fluid.each(gpii.tests.retrieveVal.perenctages, function (percentage, perIdx) {
+                var expected = gpii.tests.percentagesIfValue.outputs[valIdx][perIdx];
+                var actual = gpii.chartAuthoring.percentage.percentageIfValue(percentage, value);
+                jqUnit.assertEquals("The expected percentage is returned", expected, actual);
+            });
+        });
+
+        var defaultPercentage = 100;
+        var actualPercentage = gpii.chartAuthoring.percentage.percentageIfValue(10, null, defaultPercentage);
+        jqUnit.assertEquals("The expected defaultPercentage is returned", defaultPercentage, actualPercentage);
+    });
+
+})(jQuery, fluid);
