a5f41b19c749cea7f7cc09f9e20e26d1b7e1c310
Alan Harnum <aharnum@ocadu.ca>
2015-10-13 14:19:48 -0400

    FLOE-418: use floe namespace rather than fluid from global ID

--- src/js/pie.js
+++ src/js/pie.js
@@ -176,8 +176,8 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
             pieClass = that.classes.pie,
             titleClass = that.classes.title,
             descriptionClass = that.classes.description,
-            pieTitleId = "fluid-id-"+fluid.allocateGuid(),
-            pieDescId = "fluid-id-"+fluid.allocateGuid();
+            pieTitleId = "floe-id-"+fluid.allocateGuid(),
+            pieDescId = "floe-id-"+fluid.allocateGuid();
 
         that.arc = d3.svg.arc()
             .innerRadius(innerRadius)
