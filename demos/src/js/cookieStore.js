cc31aeafcc62a1c290431c161c699336aee454a0
Alan Harnum <aharnum@ocadu.ca>
2015-10-30 12:33:44 -0400

    FLOE-403: implement a cookieStore (similar pattern to the metadata authoring tool) to persist open/closed state of overlay between reloads

---
+++ demos/src/js/cookieStore.js
@@ -0,0 +1,36 @@
+/*
+Copyright 2015 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.txt
+*/
+
+var demo = demo || {};
+
+(function ($, fluid) {
+    "use strict";
+
+    fluid.registerNamespace("demo.chartAuthoring");
+
+    fluid.defaults("demo.chartAuthoring.cookieStore", {
+        gradeNames: ["fluid.prefs.cookieStore", "autoInit"],
+        cookie: {
+            name: "chartAuthoring-demo"
+        },
+        invokers: {
+            resetCookie: {
+                funcName: "demo.chartAuthoring.cookieStore.resetCookie",
+                args: "{that}"
+            }
+        }
+    });
+
+    demo.chartAuthoring.cookieStore.resetCookie = function (that) {
+        that.set({"showPanel": true});
+    };
+
+})(jQuery, fluid);
