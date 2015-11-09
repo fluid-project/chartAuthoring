df96fae48e5a70e0083bc8f2aca2b58bdbb68af7
Alan Harnum <aharnum@ocadu.ca>
2015-11-09 17:34:27 -0500

    FLOE-432: white outline styling for pie slice text; use Sepideh's green-scale colors

--- demos/src/js/demo.js
+++ demos/src/js/demo.js
@@ -1,5 +1,19 @@
 var demo = demo || {};
 
+/*
+
+Potential demo color stacks:
+
+Grey Scale: colors: ["#979798", "#bbbdc0", "#58585b", "#e6e7e8", "#231f20”]
+Green: colors: ["#00a59a", "#9bc863", "#1a6b61", "#e9ea7b", "#1b443d"]
+Blue: colors: ["#04ca6dc", "#65cdf5", "#546ab2", "#b6e2ec", "#20386c"]
+Red: colors: ["#f15e4e", "#faa634", "#a41c3f", "#fde95f", "#5b173b"]
+Varied#1: colors: ["#04858d", "#41beae", "#faa634", "#f15e4e", "#004861"]
+Varied#2: colors: ["#f7961c", "#87acdb", "#ffc60a", "#a2ce5a", "#f15e4e"]
+Varied#3: colors: ["#f15e4e", "#acdee4", "#73c163", "#ffc74a", "#41beae"]
+
+*/
+
 (function ($, fluid) {
 
     "use strict";
@@ -19,7 +33,8 @@ var demo = demo || {};
         },
         pieChart: {
             pieChartOptions: {
-                sliceTextDisplayTemplate: "%percentage%"
+                sliceTextDisplayTemplate: "%percentage%",
+                colors: ["#00a59a", "#9bc863", "#1a6b61", "#e9ea7b", "#1b443d"]
             }
         },
         listeners: {
