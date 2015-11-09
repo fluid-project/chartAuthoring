afe1a7e31d4be5db15a0d95b017c4bb469ee0f0d
Alan Harnum <aharnum@ocadu.ca>
2015-11-09 10:33:13 -0500

    FLOE-433: add stringtemplate behaviour for label and value cells of legend

--- src/js/legend.js
+++ src/js/legend.js
@@ -55,7 +55,22 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
             // Or, a d3 color scale that's generated based off an array of colors
             colors: null,
             sort: true, // Whether or not to sort the data by values when creating the legend
-            showLegendHeadings: true // Whether or not to display column headings in the legend
+            showLegendHeadings: true, // Whether or not to display column headings in the legend,
+            // A fluid.stringTemplate used to render the text displayed
+            // within the label cell
+            // available variables for the template are:
+            // - value: the raw value of the data
+            // - percentage: the percentage of the data value out of the total value
+            // - total: the total value of all data in the dataset
+            // - label: the attached label
+            labelTextDisplayTemplate: "%label",
+            // A fluid.stringTemplate used to render the text displayed
+            // within the value cell
+            // the same variables available as for labelTextDisplayTemplate
+            valueTextDisplayTemplate: "%value",
+            // Number of digits to display after decimal when rendering
+            // percentages for the legend
+            legendPercentageDigits: 0
         },
         styles: {
             legend: "floe-ca-pieChart-legend",
@@ -141,7 +156,9 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
     floe.chartAuthoring.pieChart.legend.updateRows = function (that) {
         var colorCellSelector = that.options.selectors.colorCell,
             labelCellSelector = that.options.selectors.labelCell,
-            valueCellSelector = that.options.selectors.valueCell;
+            valueCellSelector = that.options.selectors.valueCell,
+            labelTextDisplayTemplate = that.options.legendOptions.labelTextDisplayTemplate,
+            valueTextDisplayTemplate = that.options.legendOptions.valueTextDisplayTemplate;
 
         that.rows.each(function (d) {
             d3.select(this)
@@ -154,11 +171,15 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
 
             d3.select(this)
                 .select(labelCellSelector)
-                .text(d.label);
+                .text(function(d) {
+                    return floe.chartAuthoring.pieChart.legend.getDisplayValueFromTemplate(that, labelTextDisplayTemplate, d);
+                });
 
             d3.select(this)
                 .select(valueCellSelector)
-                .text(d.value);
+                .text(function(d) {
+                    return floe.chartAuthoring.pieChart.legend.getDisplayValueFromTemplate(that, valueTextDisplayTemplate, d);
+                });
         });
     };
 
@@ -279,6 +300,13 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
     floe.chartAuthoring.pieChart.legend.getColorArray = function (colors) {
         var colorScale = (typeof(colors) === "function") ? colors : floe.d3.getColorScale(colors);
         return colorScale.range();
+    };
+
+    floe.chartAuthoring.pieChart.legend.getDisplayValueFromTemplate = function(that, template, d) {
+        var legendPercentageDigits = that.options.legendOptions.legendPercentageDigits;
+        var percentage = floe.chartAuthoring.percentage.calculate(d.value, that.model.total.value).toFixed(legendPercentageDigits);
+        var output = fluid.stringTemplate(template, {label: d.label, value: d.value, percentage: percentage, total: that.model.total.value});
+        return output;
     };
 
 })(jQuery, fluid);
