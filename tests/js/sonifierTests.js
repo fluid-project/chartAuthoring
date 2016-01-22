4d76bf63e0597b96378f0e2e6c8b65929bae1c52
Alan Harnum <aharnum@ocadu.ca>
2016-01-22 17:00:00 -0500

    FLOE-437: add testing of sonification play and stop behaviour

--- tests/js/sonifierTests.js
+++ tests/js/sonifierTests.js
@@ -16,18 +16,39 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
     fluid.registerNamespace("floe.tests.chartAuthoring");
 
     fluid.defaults("floe.tests.chartAuthoring.sonifier", {
-        gradeNames: ["floe.chartAuthoring.sonifier", "autoInit"]
+        gradeNames: ["floe.chartAuthoring.sonifier"]
+    });
+
+    fluid.defaults("floe.tests.chartAuthoring.speedySonifier", {
+        gradeNames: ["floe.tests.chartAuthoring.sonifier"],
+        components: {
+            textToSpeech: {
+                options:{
+                    model: {
+                        utteranceOpts: {
+                            "lang": "en-GB",
+                            // Speak fast so test finishes quicker
+                            "rate": 1.9
+                        }
+                    }
+                }
+            }
+        },
+        playbackOptions: {
+            // Play fast so the test finishes quicker
+            zoom: 0.10
+        }
     });
 
     floe.tests.chartAuthoring.dataSet =
     {
         entry1: {
             value: 100,
-            label: "Label One"
+            label: "One"
         },
         entry2: {
             value: 50,
-            label: "Label Two"
+            label: "Two"
         }
     };
 
@@ -46,7 +67,7 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
             },
             percentage:67,
             value: 100,
-            label: "Label One"
+            label: "One"
         },
         {
             id: "entry2",
@@ -61,11 +82,11 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
             },
             percentage:33,
             value: 50,
-            label: "Label Two"
+            label: "Two"
         }
     ];
 
-    jqUnit.test("Test the behaviour of the sonification", function () {
+    jqUnit.test("Test the sonification data conversion", function () {
         jqUnit.expect(1);
 
         var that = floe.tests.chartAuthoring.sonifier({
@@ -75,8 +96,38 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
         });
 
         jqUnit.assertDeepEq("Sonifier's dataset is converted into the expected sonification", floe.tests.chartAuthoring.sonificationData, that.model.sonifiedData);
+    });
+
+    jqUnit.test("Test the sonification play behaviour", function () {
+        jqUnit.expect(2);
+
+        var that = floe.tests.chartAuthoring.speedySonifier({
+            model: {
+                dataSet: floe.tests.chartAuthoring.dataSet
+            },
+            listeners: {
+                "onSonificationStopped": {
+                    funcName: "floe.tests.chartAuthoring.validateStopped",
+                    args: "{that}"
+                }
+            }
+        });
 
         that.playSonification();
 
+        jqUnit.assertEquals("isPlaying boolean was true after play was started", true, that.model.isPlaying);
+        jqUnit.assertNotUndefined("Synth was created", that.model.synth);
+
     });
+
+    floe.tests.chartAuthoring.validateStopped = function(that) {
+        jqUnit.test("Test that the stop behaves as expected", function () {
+            jqUnit.expect(3);
+
+            jqUnit.assertEquals("isPlaying boolean was false after play finished", false, that.model.isPlaying);
+            jqUnit.assertEquals("currentlyPlayingData is null", null, that.model.currentlyPlayingData);
+            jqUnit.assertDeepEq("sonificationQueue is empty after play finished", [], that.model.sonificationQueue);
+        });
+    };
+
 })(jQuery, fluid);
