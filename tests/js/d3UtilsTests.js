b906a3e08f95925782b3e46d2499d1e773376d3e
Alan Harnum <aharnum@ocadu.ca>
2016-01-27 10:01:39 -0500

    FLOE-437: add tests for data ID filtering functions

--- tests/js/d3UtilsTests.js
+++ tests/js/d3UtilsTests.js
@@ -69,4 +69,30 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
         });
     });
 
+    jqUnit.test("Test floe.d3.idExtractor()", function() {
+        jqUnit.expect(3);
+
+        var cases = [{
+            msg: "Handle d.id scenario",
+            input: {id: "id1"},
+            expected: "id1"
+        },
+        {
+            msg: "Handle d.data.id scenario",
+            input: {data: {id: "id2"}},
+            expected: "id2"
+        },
+        {
+            msg: "Handle undefined scenario",
+            input: {title: "oh gosh, I am not actually a D3 'd' object"},
+            expected: undefined
+        }];
+
+        fluid.each(cases, function (oneCase) {
+            var extractedId = floe.d3.idExtractor(oneCase.input);
+            jqUnit.assertEquals(oneCase.msg, oneCase.expected, extractedId);
+        });
+
+    });
+
 })(jQuery, fluid);
