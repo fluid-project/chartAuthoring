a775e841cad38faf7a9ed798bc67f52bb7922644
Alan Harnum <waharnum@gmail.com>
2016-02-04 19:09:48 -0500

    FLOE-437: basic tests for floe.d3.filterById/toggleCSSClassById

--- tests/js/d3UtilsTests.js
+++ tests/js/d3UtilsTests.js
@@ -95,4 +95,46 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
 
     });
 
+    jqUnit.test("Test floe.d3.filterById", function () {
+        jqUnit.expect(5);
+        var dataSet = [
+            {value: 100, id: "id1"},
+            {value: 200, id: "id2"},
+            {value: 300, id: "id3"}
+        ];
+        var testRows = d3.select(".floc-filterByIdTable").selectAll(".floc-testRow");
+
+        testRows.data(dataSet);
+
+        var filteredRow = floe.d3.filterById(testRows, "id1");
+
+        jqUnit.assertEquals("Filtered row has expected length", 1, filteredRow[0].length);
+        jqUnit.assertEquals("Filtered row's first item has expected ID", "id1", filteredRow[0][0].__data__.id);
+
+        var inverseFilteredRow = floe.d3.filterById(testRows, "id1", true);
+
+        jqUnit.assertEquals("Inverse filtered row has expected length", 2, inverseFilteredRow[0].length);
+        jqUnit.assertEquals("Inverse filtered row's first item has expected ID", "id2", inverseFilteredRow[0][0].__data__.id);
+        jqUnit.assertEquals("Inverse filtered row's second item has expected ID", "id3", inverseFilteredRow[0][1].__data__.id);
+    });
+
+    jqUnit.test("Test floe.d3.toggleCSSClassById", function () {
+        var testToggleClass = "floc-testToggleClass";
+        jqUnit.expect(3);
+        var dataSet = [
+            {value: 3, id: "id1"},
+            {value: 6, id: "id2"},
+            {value: 9, id: "id3"}
+        ];
+        var testRows = d3.select(".floc-toggleCSSClassByIdTable").selectAll(".floc-testRow");
+
+        testRows.data(dataSet);
+
+        floe.d3.toggleCSSClassById(testRows, "id1", testToggleClass);
+
+        jqUnit.assertTrue("Class is toggled on to row with specified ID", testRows[0][0].className.indexOf(testToggleClass) > -1);
+        jqUnit.assertTrue("Class is not present on second row without specified ID", testRows[0][1].className.indexOf(testToggleClass) === -1);
+        jqUnit.assertTrue("Class is not present on third row without specified ID", testRows[0][2].className.indexOf(testToggleClass) === -1);
+    });
+
 })(jQuery, fluid);
