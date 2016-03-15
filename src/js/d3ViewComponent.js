d8704ef06d62e21f8eab1d50f6e0e443e62e9b69
Alan Harnum <waharnum@gmail.com>
2016-03-15 11:50:52 -0400

    GPII-1584: in midst of refactoring for svgDrawingArea grade, d3ViewComponent + svgDrawingArea unit tests passing

--- src/js/d3ViewComponent.js
+++ src/js/d3ViewComponent.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2015 OCAD University
+Copyright 2015-2016 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
@@ -32,19 +32,7 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
         },
         model: {
             // Keeps track of D3 keys and their associated DOM elements
-            dataKeys: {},
-            svgTitle: "An SVG",
-            svgDescription: "An SVG image"
-        },
-        // Options controlling the behaviour of the base SVG drawing area
-        svgOptions: {
-            width: 500,
-            height: 500
-        },
-        selectors: {
-            title: ".floec-ca-d3ViewComponent-title",
-            description: ".floec-ca-d3ViewComponent-description",
-            svg: ".floec-ca-d3ViewComponent-svg"
+            dataKeys: {}
         },
         invokers: {
             jQueryToD3: {
@@ -74,12 +62,7 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
             exitD3Elements: {
                 funcName: "floe.d3ViewComponent.exitD3Elements",
                 args: ["{arguments}.0", "{that}"]
-            },
-            createBaseSVGDrawingArea: {
-                funcName: "floe.d3ViewComponent.createBaseSVGDrawingArea",
-                args: ["{that}"]
             }
-
         }
     });
 
@@ -231,65 +214,6 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
         });
 
         d3ExitSelection.remove();
-    };
-
-    // Returns a properly formatted viewBox attribute that helps in making
-    // SVG elements scalable
-    // https://sarasoueidan.com/blog/svg-coordinate-systems/ has a lengthy
-    // explanation
-
-    floe.d3ViewComponent.getViewBoxConfiguration = function (x, y, width, height) {
-        return x + "," + y + "," + width + "," + height;
-    };
-
-    // Given width, height and class, creates an initial SVG to draw in,
-    // and appends tags and attributes for alternative representation
-    floe.d3ViewComponent.createBaseSVGDrawingArea = function (that) {
-        var container = that.container,
-            width = that.options.svgOptions.width,
-            height = that.options.svgOptions.height,
-            titleClass = that.classes.title,
-            descriptionClass = that.classes.description,
-            svgClass = that.classes.svg;
-
-        that.svg = that.jQueryToD3(container)
-            .append("svg")
-            .attr({
-                "width": width,
-                "height": height,
-                "class": svgClass,
-                "viewBox": floe.d3ViewComponent.getViewBoxConfiguration(0, 0, width, height),
-                // Set aria role to image - this causes the chart to appear as a
-                // static image to AT rather than as a number of separate
-                // images
-                "role": "img"
-            });
-
-        that.svg
-            .append("title")
-            .attr({
-                "class": titleClass
-            })
-            .text(that.model.svgTitle);
-
-        // Allocate ID for the title element
-        var svgTitleId = fluid.allocateSimpleId(that.locate("title"));
-
-        that.svg
-            .append("desc")
-            .attr({
-                "class": descriptionClass
-            })
-            .text(that.model.svgDescription);
-
-        // Allocate ID for the desc element
-        var svgDescId = fluid.allocateSimpleId(that.locate("description"));
-
-        // Now that they've been created and have IDs, explicitly associate SVG
-        // title & desc via aria-labelledby
-        that.svg.attr({
-            "aria-labelledby": svgTitleId + " " + svgDescId
-        });
     };
 
 })(jQuery, fluid);
